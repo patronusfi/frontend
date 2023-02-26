038a4778c73923ebf614e20db949c583f50d0b09
showlizi <showlizi@outlook.com>
2023-02-26 12:39:51 +0800

    feat: add i18n

---
+++ i18n/en.js
@@ -0,0 +1,37 @@
+
+
+const loginModal = {
+  loginModalTitle: 'Choose a wallet',
+  loginModalDescribe: 'Please DYOR before using and use at your own risk.',
+  loginModalFaucet: 'Faucet'
+}
+
+const header = {
+  marketOverview: 'Market Overview',
+  depositWithdraw: 'Deposit/Withdraw',
+  borrowRepay: 'Borrow/Repay',
+  dashboard: 'Dashboard',
+  faucet: 'Faucet',
+  disconnect: 'Disconnect',
+  connectWallet: 'Connect Wallet',
+  whitePaper: 'White paper',
+  feedback: 'Feedback',
+}
+
+const indexPage = {
+  ...header,
+  totalDeposit: 'Total Deposit',
+  totalBorrow: 'Total Borrowing',
+  indexTableTitleMarket: 'Market',
+  indexTableTitleAmount: 'Amount',
+  indexTableTheadAssets: 'Asset',
+  indexTableTheadDepositAPY: 'Deposit APY',
+  indexTableTheadBorrowAPY: 'Borrow APY',
+  indexTableTheadWalletBalance: 'Wallet Balance',
+}
+
+export default {
+  "Welcome to React": " lizi",
+  ...loginModal,
+  ...indexPage
+}
---
+++ i18n/ko.js
@@ -0,0 +1,35 @@
+const loginModal = {
+  "loginModalTitle": "지갑 선택",
+  "loginModalDescribe": "사용하기 전에 충분한 연구를 진행하시고, 본인 책임 하에 사용해주세요.",
+  "loginModalFaucet": "수도꼭지"
+}
+
+const header = {
+  "marketOverview": "시장 개요",
+  "depositWithdraw": "입출금",
+  "borrowRepay": "차입/상환",
+  "dashboard": "대시보드",
+  "faucet": "수도꼭지",
+  "disconnect": "연결 해제",
+  "connectWallet": "지갑 연결",
+  "whitePaper": "백서",
+  'feedback': '피드백',
+}
+
+const indexPage = {
+  ...header,
+  "totalDeposit": "총 입금액",
+  "totalBorrow": "총 대출액",
+  "indexTableTitleMarket": "시장",
+  "indexTableTitleAmount": "금액",
+  "indexTableTheadAssets": "자산",
+  "indexTableTheadDepositAPY": "입금 APY",
+  "indexTableTheadBorrowAPY": "대출 APY",
+  "indexTableTheadWalletBalance": "지갑 잔고"
+}
+
+export default {
+  "Welcome to React": " Bienvenue à React et lizi",
+  ...loginModal,
+  ...indexPage
+}
